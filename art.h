6e7f6a5cf5d8184c7ca0dd05711da1f5a367ec46
Chi Nguyen <buichinguyen1105@gmail.com>
2026-01-22 16:45:57 +0200

    add comments to .h

--- art.h
+++ art.h
@@ -14,8 +14,15 @@ art_tree *art_new(void);
 // Destroy the tree.
 void art_free(art_tree *t);
 
+// Insert a key-value pair
+// Key: binary string
+// Value: pointer to the data you want to store
+// Returns: NULL if inserted new, or old values if its a update
 void *art_insert(art_tree *t, const unsigned char *key, int len, void *value);
 
+// Delete a key
+// Returns: the pointer to the deleted item (you have ownership to free it)
+// or NULL if key doesn't exist
 void *art_delete(art_tree *t, const unsigned char *key, int len);
 
 #endif
